0122430896ba7c74bf11aac6bd2d8af6b83bca1c
qrlk <hi@qrlk.me>
2021-10-01 20:02:40 +0300

    duplicate commands in the last chat id

--- src/background.js
+++ src/background.js
@@ -32,6 +32,7 @@ chrome.commands.onCommand.addListener(function (command) {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { command: command });
       });
+      chrome.tabs.sendMessage(chatId, { command: command });
       break;
   }
 });
