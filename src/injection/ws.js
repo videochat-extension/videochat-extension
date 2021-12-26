4aabc146ebeab14301a997a0572fd381ca22355d
qrlk <hi@qrlk.me>
2021-12-26 22:46:13 +0300

    avoid 'your interlocutor has denied access.."

--- src/injection/ws.js
+++ src/injection/ws.js
@@ -42,9 +42,10 @@ TextEncoder.prototype.encode = function (data) {
             //     }
             //     return this._encode(prefix + JSON.stringify(js));
 
-            case "WRD":
-                js["Data"]["hasStream"] = false
-                return this._encode(prefix + JSON.stringify(js));
+            //case "WRD":
+                //js["Data"]["hasStream"] = "asdasd"
+				//delete js["Data"]["hasStream"]
+                //return this._encode(prefix + JSON.stringify(js));
             case "FIL":
                 CO = js["Country"]
                 return this._encode(data);
