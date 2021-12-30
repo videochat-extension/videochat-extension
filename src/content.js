b145d15df785d468dccbba535e5afeb40e3e019b
qrlk <hi@qrlk.me>
2021-12-30 13:21:47 +0300

    move hotkeys from content.js to hotkeys.js

--- src/content.js
+++ src/content.js
@@ -77,35 +77,6 @@ function confirmAndReload() {
     }
 }
 
-function hotkeys(e) {
-    if (e.srcElement.className === "emojionearea-editor")
-        return
-
-    switch (e.key) {
-        case "ArrowLeft":
-            if (document.getElementById("report-popup").style.display === "block")
-                document.getElementsByClassName("btn btn-gray")[2].click()
-            else if (e.shiftKey && !local.ips.includes(remoteIP.innerText)) {
-                syncBlackList()
-            }
-            document.getElementsByClassName('buttons__button start-button')[0].click()
-            break;
-
-        case "ArrowUp":
-            document.getElementsByClassName('buttons__button stop-button')[0].click()
-            break;
-
-        case "ArrowDown":
-            document.getElementsByClassName("message-report-link tr")[0].click()
-            break;
-
-        case "ArrowRight":
-            if (document.getElementById("report-popup").style.display === "block")
-                document.getElementsByClassName("btn btn-main send-report")[1].click()
-            break;
-    }
-}
-
 function syncBlackList() {
     if (settings.dontBanMobile) {
         if (!JSON.parse(remoteIPInfo.innerText).mobile) {
@@ -644,53 +615,6 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
             settings = result;
         });
 });
-
-chrome.runtime.onMessage.addListener(
-    function (request) {
-        switch (request.command) {
-            case "skip":
-                document.getElementsByClassName('buttons__button start-button')[0].click()
-                break;
-
-            case "skip_ban":
-                if (!local.ips.includes(remoteIP.innerText)) {
-                    syncBlackList()
-                }
-
-                document.getElementsByClassName('buttons__button start-button')[0].click()
-                break;
-
-
-            case "stop":
-                document.getElementsByClassName('buttons__button stop-button')[0].click()
-                break;
-
-            case "screen_remote":
-                dwncanvas = document.createElement('canvas');
-                dwncanvas.width = document.getElementById('remote-video').videoWidth
-                dwncanvas.height = document.getElementById('remote-video').videoHeight
-
-                var ctx = dwncanvas.getContext('2d');
-
-                ctx.drawImage(document.getElementById("remote-video"), 0, 0, dwncanvas.width, dwncanvas.height);
-                downloadImage(dwncanvas.toDataURL('image/jpg'))
-                dwncanvas = null
-                break;
-
-            case "screen_local":
-                dwncanvas = document.createElement('canvas');
-                dwncanvas.width = document.getElementById('local-video').videoWidth
-                dwncanvas.height = document.getElementById('local-video').videoHeight
-
-                var ctx = dwncanvas.getContext('2d');
-
-                ctx.drawImage(document.getElementById("local-video"), 0, 0, dwncanvas.width, dwncanvas.height);
-                downloadImage(dwncanvas.toDataURL('image/jpg'))
-                dwncanvas = null
-                break;
-        }
-    }
-);
 
 $(document).arrive(".ban-popup__unban_msg.tr", function (el) {
     Arrive.unbindAllArrive();
