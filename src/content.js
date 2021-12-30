4c08aafc44c897f905de6c47a638d15a3655f336
qrlk <hi@qrlk.me>
2021-12-30 11:38:38 +0300

    ws settings section change

--- src/content.js
+++ src/content.js
@@ -1632,132 +1632,135 @@ function createSettingsRisky() {
                 })
             ]),
         ]),
-        createElement('br'),
-        createElement('dd', {}, [
-            createElement('span', {}, [
-                createElement("p", {
-                    innerText: chrome.i18n.getMessage("skipSoundWS"),
-                    className: "tooltip",
-                    title: chrome.i18n.getMessage("tooltipSkipSoundWS")
-                }),
-                createElement('input', {
-                    type: "checkbox",
-                    checked: settings.wsconfig.theyskipsound,
-                    id: "wsconfigtheyskipsoundCheck",
-                    onclick: () => {
-                        settings.wsconfig.theyskipsound = wsconfigtheyskipsoundCheck.checked
-                        chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
 
-                        });
-                    }
-                })
+        createElement('dd', {}, [
+            createElement('br'),
+            createElement('dd', {}, [
+                createElement('span', {}, [
+                    createElement("p", {
+                        innerText: chrome.i18n.getMessage("skipSoundWS"),
+                        className: "tooltip",
+                        title: chrome.i18n.getMessage("tooltipSkipSoundWS")
+                    }),
+                    createElement('input', {
+                        type: "checkbox",
+                        checked: settings.wsconfig.theyskipsound,
+                        id: "wsconfigtheyskipsoundCheck",
+                        onclick: () => {
+                            settings.wsconfig.theyskipsound = wsconfigtheyskipsoundCheck.checked
+                            chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
+
+                            });
+                        }
+                    })
+                ]),
             ]),
-        ]),
 
-        createElement('dd', {}, [
-            createElement('span', {}, [
-                createElement("p", {
-                    innerText: chrome.i18n.getMessage("skipWrongCountry"),
-                    className: "tooltip",
-                    title: chrome.i18n.getMessage("tooltipSkipWrongCountry")
-                }),
-                createElement('input', {
-                    type: "checkbox",
-                    checked: settings.wsconfig.skipwrongcountry,
-                    id: "wsconfigskipwrongcountryCheck",
-                    onclick: () => {
-                        settings.wsconfig.skipwrongcountry = wsconfigskipwrongcountryCheck.checked
-                        chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
+            createElement('dd', {}, [
+                createElement('span', {}, [
+                    createElement("p", {
+                        innerText: chrome.i18n.getMessage("skipWrongCountry"),
+                        className: "tooltip",
+                        title: chrome.i18n.getMessage("tooltipSkipWrongCountry")
+                    }),
+                    createElement('input', {
+                        type: "checkbox",
+                        checked: settings.wsconfig.skipwrongcountry,
+                        id: "wsconfigskipwrongcountryCheck",
+                        onclick: () => {
+                            settings.wsconfig.skipwrongcountry = wsconfigskipwrongcountryCheck.checked
+                            chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
 
-                        });
-                    }
-                })
+                            });
+                        }
+                    })
+                ]),
             ]),
-        ]),
 
-        createElement('dd', {}, [
-            createElement('span', {}, [
-                createElement("p", {
-                    innerText: chrome.i18n.getMessage("replacePreview"),
-                    className: "tooltip",
-                    title: chrome.i18n.getMessage("tooltipReplacePreview")
-                }),
-                createElement('input', {
-                    type: "checkbox",
-                    checked: settings.wsconfig.replacePic,
-                    id: "wsconfigreplacePicCheck",
-                    onclick: () => {
-                        settings.wsconfig.replacePic = wsconfigreplacePicCheck.checked
-                        chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
+            createElement('dd', {}, [
+                createElement('span', {}, [
+                    createElement("p", {
+                        innerText: chrome.i18n.getMessage("replacePreview"),
+                        className: "tooltip",
+                        title: chrome.i18n.getMessage("tooltipReplacePreview")
+                    }),
+                    createElement('input', {
+                        type: "checkbox",
+                        checked: settings.wsconfig.replacePic,
+                        id: "wsconfigreplacePicCheck",
+                        onclick: () => {
+                            settings.wsconfig.replacePic = wsconfigreplacePicCheck.checked
+                            chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
 
-                        });
-                    }
-                })
+                            });
+                        }
+                    })
+                ]),
             ]),
-        ]),
 
-        createElement('dd', {}, [
-            createElement('span', {}, [
-                createElement("p", {
-                    innerText: chrome.i18n.getMessage("deletePreview"),
-                    className: "tooltip",
-                    title: chrome.i18n.getMessage("tooltipDeletePreview")
-                }),
-                createElement('input', {
-                    type: "checkbox",
-                    checked: settings.wsconfig.deletePic,
-                    id: "wsconfigdeletePicCheck",
-                    onclick: () => {
-                        settings.wsconfig.deletePic = wsconfigdeletePicCheck.checked
-                        chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
+            createElement('dd', {}, [
+                createElement('span', {}, [
+                    createElement("p", {
+                        innerText: chrome.i18n.getMessage("deletePreview"),
+                        className: "tooltip",
+                        title: chrome.i18n.getMessage("tooltipDeletePreview")
+                    }),
+                    createElement('input', {
+                        type: "checkbox",
+                        checked: settings.wsconfig.deletePic,
+                        id: "wsconfigdeletePicCheck",
+                        onclick: () => {
+                            settings.wsconfig.deletePic = wsconfigdeletePicCheck.checked
+                            chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
 
-                        });
-                    }
-                })
+                            });
+                        }
+                    })
+                ]),
             ]),
-        ]),
 
 
-        createElement('dd', {}, [
-            createElement('span', {}, [
-                createElement("p", {
-                    innerText: chrome.i18n.getMessage("replaceReportPic"),
-                    className: "tooltip",
-                    title: chrome.i18n.getMessage("tooltipReplaceReportPic")
-                }),
-                createElement('input', {
-                    type: "checkbox",
-                    checked: settings.wsconfig.replaceReportPics,
-                    id: "wsconfigreplaceReportPicsCheck",
-                    onclick: () => {
-                        settings.wsconfig.replaceReportPics = wsconfigreplaceReportPicsCheck.checked
-                        chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
+            createElement('dd', {}, [
+                createElement('span', {}, [
+                    createElement("p", {
+                        innerText: chrome.i18n.getMessage("replaceReportPic"),
+                        className: "tooltip",
+                        title: chrome.i18n.getMessage("tooltipReplaceReportPic")
+                    }),
+                    createElement('input', {
+                        type: "checkbox",
+                        checked: settings.wsconfig.replaceReportPics,
+                        id: "wsconfigreplaceReportPicsCheck",
+                        onclick: () => {
+                            settings.wsconfig.replaceReportPics = wsconfigreplaceReportPicsCheck.checked
+                            chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
 
-                        });
-                    }
-                })
+                            });
+                        }
+                    })
+                ]),
             ]),
-        ]),
 
 
-        createElement('dd', {}, [
-            createElement('span', {}, [
-                createElement("p", {
-                    innerText: chrome.i18n.getMessage("deleteReportPic"),
-                    className: "tooltip",
-                    title: chrome.i18n.getMessage("tooltipDeleteReportPic")
-                }),
-                createElement('input', {
-                    type: "checkbox",
-                    checked: settings.wsconfig.deleteReportPics,
-                    id: "wsconfigdeleteReportPicsCheck",
-                    onclick: () => {
-                        settings.wsconfig.deleteReportPics = wsconfigdeleteReportPicsCheck.checked
-                        chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
+            createElement('dd', {}, [
+                createElement('span', {}, [
+                    createElement("p", {
+                        innerText: chrome.i18n.getMessage("deleteReportPic"),
+                        className: "tooltip",
+                        title: chrome.i18n.getMessage("tooltipDeleteReportPic")
+                    }),
+                    createElement('input', {
+                        type: "checkbox",
+                        checked: settings.wsconfig.deleteReportPics,
+                        id: "wsconfigdeleteReportPicsCheck",
+                        onclick: () => {
+                            settings.wsconfig.deleteReportPics = wsconfigdeleteReportPicsCheck.checked
+                            chrome.storage.sync.set({"wsconfig": settings.wsconfig}, function () {
 
-                        });
-                    }
-                })
+                            });
+                        }
+                    })
+                ]),
             ]),
         ]),
     ])
