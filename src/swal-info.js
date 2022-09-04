8f98a6415a5bbaa8a191ca2405dacc6f449ed8e7
qrlk <hi@qrlk.me>
2022-09-05 01:55:47 +0700

    apply changelog improvements to the 'welcome window'
    - avoid flickering through Swal.update() and custom button events
    - clickable progressSteps
    - arrow hotkeys
    - min height, center each step content

--- src/swal-info.js
+++ src/swal-info.js
@@ -1,23 +1,5 @@
 const showSwalInfo = async function () {
     const steps = ['1', '2', '3', '4', '5', '6', '7']
-    const swalQueueStep = Swal.mixin({
-        progressSteps: steps,
-        heightAuto: false,
-        confirmButtonText: chrome.i18n.getMessage('confirmButtonText'),
-        cancelButtonText: chrome.i18n.getMessage('cancelButtonText'),
-        showDenyButton: true,
-        denyButtonText: chrome.i18n.getMessage('denyButtonText'),
-        preDeny: () => {
-            chrome.storage.sync.set({"swalInfoCompleted": true})
-        },
-        allowOutsideClick: false,
-        allowEnterKey: true,
-        reverseButtons: true,
-
-        // TODO: FIX FLICKERING ON MODAL CHANGE
-        showClass: {popup: 'swal2-noanimation', backdrop: 'swal2-noanimation'},
-        hideClass: {backdrop: 'swal2-noanimation'}
-    })
 
     const titles = [
         chrome.i18n.getMessage("swalInfoTitle1"),
@@ -36,7 +18,7 @@ const showSwalInfo = async function () {
         chrome.i18n.getMessage("swalInfoText4"),
         chrome.i18n.getMessage("swalInfoText5"),
         chrome.i18n.getMessage("swalInfoText6"),
-        `<div style="max-height: 40vh">MIT License<br><br>
+        `<div style="max-height: 300px">MIT License<br><br>
 
 Copyright (c) 2021-2022 <a target=\"_blank\" style=\"text-decoration: none!important;\" href=\"http://qrlk.me\">Fyodor Kurlyuk</a><br><br>
 
@@ -58,26 +40,105 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.</div>`
     ]
-    let currentStep
 
-    for (currentStep = 0; currentStep < steps.length;) {
-        const result = await swalQueueStep.fire({
+    let currentStep = 0
+
+    const swalQueueStep = Swal.mixin({
+        // disable animation
+        showClass: {popup: 'swal2-noanimation', backdrop: 'swal2-noanimation'},
+        hideClass: {backdrop: 'swal2-noanimation'},
+        allowOutsideClick: false,
+        allowEnterKey: true,
+        showDenyButton: true,
+        preDeny: () => {
+            chrome.storage.sync.set({"swalInfoCompleted": true})
+        },
+        confirmButtonText: chrome.i18n.getMessage('confirmButtonText'),
+        denyButtonText: chrome.i18n.getMessage('denyButtonText'),
+        cancelButtonText: chrome.i18n.getMessage('cancelButtonText'),
+        heightAuto: false,
+        reverseButtons: true,
+        progressSteps: steps,
+    })
+
+    const selectStep = function (step) {
+        swalQueueStep.update({
             title: titles[currentStep],
-            html: values[currentStep],
+            html: `<div style="min-height: 300px;align-items: center;display: flex;justify-content: center;"><div>${values[currentStep]}</div></div>`,
             showCancelButton: currentStep > 0,
-            currentProgressStep: currentStep
+            currentProgressStep: currentStep,
         })
+    }
 
-        if (result.value) {
-            currentStep++
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-            currentStep--
-        } else {
-            break
+    const arrowHotkeys = function (e) {
+        switch (e.key) {
+            case "ArrowLeft":
+                if (currentStep !== 0) {
+                    Swal.getCancelButton().click()
+                    Swal.getCancelButton().focus()
+                } else {
+                    Swal.getConfirmButton().focus()
+                }
+                break;
+
+            case "ArrowUp":
+                Swal.getDenyButton().click()
+                break;
+
+            case "ArrowRight":
+                Swal.getConfirmButton().click()
+                Swal.getConfirmButton().focus()
+                break;
         }
+        e.preventDefault()
     }
 
-    if (currentStep === steps.length) {
-        chrome.storage.sync.set({"swalInfoCompleted": true})
-    }
+    const result = await swalQueueStep.fire(
+        {
+            title: titles[currentStep],
+            html: `<div style="min-height: 300px;align-items: center;display: flex;justify-content: center;"><div>${values[currentStep]}</div></div>`,
+            showCancelButton: currentStep > 0,
+            currentProgressStep: currentStep,
+
+            willOpen: (e) => {
+                e.querySelector('.swal2-cancel').onclick = (e) => {
+                    if (currentStep - 1 >= 0) {
+                        currentStep = currentStep - 1
+                        selectStep(currentStep)
+                        Swal.getCancelButton().focus()
+                    } else {
+                        Swal.close()
+                    }
+                }
+                e.querySelector('.swal2-confirm').onclick = (e) => {
+                    if (currentStep + 1 < steps.length) {
+                        currentStep = currentStep + 1
+                        selectStep(currentStep)
+                        Swal.getConfirmButton().focus()
+                    } else {
+                        Swal.close()
+                        chrome.storage.sync.set({"swalInfoCompleted": true})
+                    }
+                }
+            },
+            didOpen: () => {
+                document.removeEventListener('keyup', arrowHotkeys)
+                document.addEventListener('keyup', arrowHotkeys)
+            },
+            didRender: () => {
+                let progressSteps = $(".swal2-progress-step")
+                progressSteps.css({
+                    "user-select": "none",
+                    'cursor': 'pointer'
+                })
+                progressSteps.click(function (el) {
+                    currentStep = steps.indexOf(el.target.innerText)
+                    selectStep(currentStep)
+                })
+            },
+            willClose: () => {
+                document.removeEventListener('keyup', arrowHotkeys)
+            }
+        }
+    )
 }
