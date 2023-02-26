b37c13b5f4689ae977210fba0ad09bdbd6d48f20
qrlk <hi@qrlk.me>
2023-02-26 11:41:18 +0700

    use Proxy api in ip-api.ts

--- src/injection/ip-api.ts
+++ src/injection/ip-api.ts
@@ -1,11 +1,5 @@
-// based on magic from https://github.com/fippo/rtcstats (MIT)
-let nativeMethod = window.RTCPeerConnection.prototype.addIceCandidate
-if (nativeMethod) {
-    window.RTCPeerConnection.prototype.addIceCandidate = function () {
-        let pc = this;
-        let args = arguments;
-
-        // if something fails, the videochat behaviour should not break, so try-catch block in this case is fine
+const addIceCandidateHandler = {
+    apply(target: any, thisArg: any, args: any) {
         try {
             // firefox does not return .address / .type when creating RTCIceCandidate in the content script,
             // so here is a workaround for this issue
@@ -25,22 +19,11 @@ if (nativeMethod) {
             console.dir("ERROR: Failed to pass addIceCandidate data to the content script");
             console.dir(e);
         }
-
-        return new Promise(function (resolve, reject) {
-            nativeMethod.apply(pc, [args[0],
-                function () {
-                    resolve();
-                    if (args.length >= 2) {
-                        args[1].apply(null, []);
-                    }
-                },
-                function (err) {
-                    reject(err);
-                    if (args.length >= 3) {
-                        args[2].apply(null, [err]);
-                    }
-                }]
-            );
-        });
-    };
+        return Reflect.apply(target, thisArg, args)
+    },
 }
+
+RTCPeerConnection.prototype.addIceCandidate = new Proxy(
+    RTCPeerConnection.prototype.addIceCandidate,
+    addIceCandidateHandler,
+)
