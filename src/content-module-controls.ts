62b1dbdfeea5c86b483a1a2dec2b82ff46d6cb74
qrlk <hi@qrlk.me>
2023-02-26 21:11:54 +0700

    move the createrHeader to the controls module

--- src/content-module-controls.ts
+++ src/content-module-controls.ts
@@ -1,7 +1,6 @@
 import {ChatruletkaDriver} from "./content-driver-chatruletka";
 import * as utils from "./utils";
 import $ from "jquery";
-import {createHeader} from "./content-controls-header";
 import {mapModule} from "./content-module-controls-map";
 import {ContentSwalChangelog} from "./content-swal-changelog";
 import {ContentSwalInfo} from "./content-swal-info";
@@ -417,4 +416,114 @@ export class ControlsTabAbout {
             })
         ])
     }
+}
+function createHeader() {
+    return utils.createElement('center', {
+        style: "user-select:none"
+    }, [
+        utils.createElement('div', {
+            style: "position:absolute; left:0;top:0",
+        }, [
+            utils.createElement('button', {
+                style: "color: red; height:15px",
+                title: chrome.i18n.getMessage("screen_remote"),
+                onclick: () => {
+                    let dwncanvas = document.createElement('canvas');
+                    dwncanvas.width = (document.getElementById('remote-video') as HTMLVideoElement)?.videoWidth
+                    dwncanvas.height = (document.getElementById('remote-video') as HTMLVideoElement)?.videoHeight
+
+                    let ctx = dwncanvas.getContext('2d');
+                    if (ctx instanceof CanvasRenderingContext2D) {
+                        ctx.drawImage((document.getElementById('remote-video') as HTMLVideoElement), 0, 0, dwncanvas.width, dwncanvas.height);
+                        utils.downloadImage(dwncanvas.toDataURL('image/jpg'))
+                    }
+                },
+            }, [
+                utils.createElement('b', {
+                    innerText: "^"
+                })
+            ]),
+            utils.createElement('button', {
+                style: "color: green; height:15px",
+                title: "pip remote",
+                onclick: () => {
+                    if (document.pictureInPictureElement === document.getElementById("remote-video"))
+                        document.exitPictureInPicture()
+                    else
+                        (document.getElementById("remote-video") as HTMLVideoElement).requestPictureInPicture()
+                },
+            }, [
+                utils.createElement('b', {
+                    innerText: "^"
+                })
+            ]),
+            utils.createElement('button', {
+                style: function f() {
+                    if (globalThis.settings.streamer && globalThis.settings.streamerPip) {
+                        return "height:15px"
+                    } else {
+                        return "display:none"
+                    }
+                }(),
+                title: "pip remote clone (for streamers)",
+                onclick: () => {
+                    if (document.pictureInPictureElement === document.getElementById("echo-video"))
+                        document.exitPictureInPicture()
+                    else
+                        (document.getElementById("echo-video") as HTMLVideoElement).requestPictureInPicture()
+                },
+            }, [
+                utils.createElement('b', {
+                    innerText: "^"
+                })
+            ]),
+        ]),
+        utils.createElement('a', {
+            target: "_blank",
+            style: "text-decoration: none!important; color: #000000;",
+            href: "https://chrome.google.com/webstore/detail/alchldmijhnnapijdmchpkdeikibjgoi"
+        }, [
+            utils.createElement('b', {
+                innerText: chrome.i18n.getMessage("extension_name").replace(" (ome.tv) ", " ") + " v" + chrome.runtime.getManifest().version.substring(0, 3),
+                id: "connectionStatus",
+            })
+        ]),
+        utils.createElement('div', {
+            style: "position:absolute; right:0; top:0",
+        }, [
+            utils.createElement('button', {
+                style: "color: green; height:15px",
+                title: "pip local",
+                onclick: () => {
+                    if (document.pictureInPictureElement === document.getElementById("local-video"))
+                        document.exitPictureInPicture()
+                    else
+                        (document.getElementById("local-video") as HTMLVideoElement).requestPictureInPicture()
+                },
+            }, [
+                utils.createElement('b', {
+                    innerText: "^"
+                })
+            ]),
+            utils.createElement('button', {
+                style: "color: red; height:15px",
+                title: chrome.i18n.getMessage("screen_local"),
+                onclick: () => {
+                    let dwncanvas = document.createElement('canvas');
+                    dwncanvas.width = (document.getElementById('local-video') as HTMLVideoElement)?.videoWidth
+                    dwncanvas.height = (document.getElementById('local-video') as HTMLVideoElement)?.videoHeight
+
+                    let ctx = dwncanvas.getContext('2d');
+                    if (ctx instanceof CanvasRenderingContext2D) {
+                        ctx.drawImage((document.getElementById('local-video') as HTMLVideoElement), 0, 0, dwncanvas.width, dwncanvas.height);
+                        utils.downloadImage(dwncanvas.toDataURL('image/jpg'))
+                    }
+                },
+            }, [
+                utils.createElement('b', {
+                    innerText: "^"
+                })
+            ]),
+        ]),
+    ])
 }
