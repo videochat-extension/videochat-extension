e67fb404b3139c8fafaf637cfcba0ea2bc236a58
qrlk <hi@qrlk.me>
2023-02-26 17:16:35 +0700

    DRY checkbox in settings-misc

--- src/content-module-settings-misc.ts
+++ src/content-module-settings-misc.ts
@@ -6,26 +6,7 @@ export function createSettingsMisc() {
     return utils.createElement('div', {}, [
         ControlsTabSettings.createSettingsHeader(chrome.i18n.getMessage("settingsMisc")),
 
-        utils.createElement('dd', {}, [
-            utils.createElement('span', {}, [
-
-                utils.createElement("p", {
-                    innerText: chrome.i18n.getMessage("sentry"),
-                    className: "tooltip",
-                    title: chrome.i18n.getMessage("tooltipSentry")
-                }),
-                utils.createElement('input', {
-                    type: "checkbox",
-                    checked: globalThis.settings.sentry,
-                    style: "margin",
-                    id: "sentryCheck",
-                    onclick: () => {
-                        chrome.storage.sync.set({"sentry": (document.getElementById("sentryCheck") as HTMLInputElement).checked});
-                    }
-                })
-            ]),
-        ]),
-        // TODO: DRY
+        ControlsTabSettings.createSettingsCheckbox("p", "sentry", chrome.i18n.getMessage("sentry"), chrome.i18n.getMessage("tooltipSentry")),
 
         ControlsTabSettings.createSettingsButton(chrome.i18n.getMessage("switchModeButtonText"), () => {
             switchMode()
