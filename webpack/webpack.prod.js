0662f60af8b3196b36cc0d01f9e6e509a4a88ca2
qrlk <hi@qrlk.me>
2023-03-26 14:50:15 +0700

    upload maps for sentry

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,17 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    devtool: "source-map",
+    plugins: [
+        new SentryWebpackPlugin({
+            org: "cool-projects",
+            project: "videochatru-extension",
+            include: process.env.DIR,
+            authToken: process.env.SENTRY_TOKEN,
+            release: `videochat-extension@${process.env.VERSION}`,
+        }),
+    ],
 });
