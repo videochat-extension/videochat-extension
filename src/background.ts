66d9ffd2e9f1e71fda04b154196f26dcf4737ab9
qrlk <hi@qrlk.me>
2023-03-26 11:56:28 +0700

    reformat code

--- src/background.ts
+++ src/background.ts
@@ -366,7 +366,9 @@ function runtimeOnMessage(request: any, sender: chrome.runtime.MessageSender, se
                 } else {
                     sendResponse({status: response.status, body: {}})
                 }
-            }).catch((error) => {sendResponse({status: 0, body: `${error.message}`})})
+            }).catch((error) => {
+            sendResponse({status: 0, body: `${error.message}`})
+        })
 
         return true;
     }
