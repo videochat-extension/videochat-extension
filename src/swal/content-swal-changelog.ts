e50478b8b5a4df0be08a44ef1487671f7466b6c3
qrlk <hi@qrlk.me>
2023-04-13 03:13:26 +0700

    upd changelog

--- src/swal/content-swal-changelog.ts
+++ src/swal/content-swal-changelog.ts
@@ -1483,6 +1483,7 @@ let changelog: { version: string, date: string, description: { en: string, ru: s
                 '<b>- Fixes & Improvements.</b><br>' +
                 '— Fixed shift+arrow-left ban hotkey (Chatruletka, Ome.tv, Minichat, Chatrulez).<br>' +
                 '— Fixed \'autoskip long loading\' automation feature.<br>' +
+                '— Fixed detection of the size of the container with information about the location of the interlocutor when displaying the results of gender recognition or the status of the streamer mode.<br>' +
                 '— Fixed spinner color in omegle dark theme.<br>' +
                 "— Fixed zoom in the 'Missing permission detected' window.<br>" +
                 "— Post-uninstall survey now opens on the <a style=\"text-decoration:none;\" target=\"_blank\" href=\"https://videochat-extension.starbase.wiki/en\">extension's website</a>.",
@@ -1500,6 +1501,7 @@ let changelog: { version: string, date: string, description: { en: string, ru: s
                 '<b>- Исправления и улучшения.</b><br>' +
                 '— Исправлена работа клавиши shift+стрелка-влево (Chatruletka, Ome.tv, Minichat, Chatrulez).<br>' +
                 '— Исправлена работа функции \'автопропуск долгой загрузки\'.<br>' +
+                '— Исправлено определение размера контейнера с информацией о местоположении собеседника при отображении статуса распознавания пола или статуса режима стримера.<br>' +
                 '— Исправлен цвет спиннера при включённой тёмной теме (Omegle).<br>' +
                 '— Исправлен размер окошка \'Обнаружено отсутствующее разрешение\'.<br>' +
                 "— Опрос после удаления теперь открывается на <a style=\"text-decoration:none;\" target=\"_blank\" href=\"https://videochat-extension.starbase.wiki/ru\">сайте расширения</a>.",
