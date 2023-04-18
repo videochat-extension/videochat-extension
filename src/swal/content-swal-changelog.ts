ea1eca638a16deb257f0dd7c0c6dce7da667f932
Fyodor Kurlyuk <qrluke@proton.me>
2023-04-19 00:07:13 +0700

    upd changelog

--- src/swal/content-swal-changelog.ts
+++ src/swal/content-swal-changelog.ts
@@ -1532,6 +1532,7 @@ let changelog: { version: string, date: string, description: { en: string, ru: s
                 '<b>- Dev.</b><br>' +
                 '— Faceapi is now loaded only when needed, which improves the launch speed of the extension on very old computers.<br>' +
                 '— Slightly reduced the size of the extension.<br>' +
+                '— Added caching of popup content and height. The popup loads just as slowly (0.1s-0.3s), but it\'s less noticeable.<br>' +
                 '— Improved error collection functionality, which will allow us to identify and fix errors faster than before.<br>' +
                 '— Changed the way the extension is compiled, which in theory should significantly speed up the approval process for future updates by stores.',
             ru: '<b>Скучный maintence релиз.</b><br>' +
@@ -1539,6 +1540,7 @@ let changelog: { version: string, date: string, description: { en: string, ru: s
                 '<b>- Скучные буквы.</b><br>' +
                 '— Faceapi теперь загружается только при необходимости, что сильно ускоряет запуск расширения на очень слабых компьютерах.<br>' +
                 '— Немного уменьшен размер расширения.<br>' +
+                '— Добавлено кэширование содержимого и высоты попапа. Попап загружается так же медленно, но это менее заметно.<br>' +
                 '— Улучшен функционал сбора ошибок, что позволит быстрее нам определять и исправлять ошибки, чем раньше.<br>' +
                 '— Изменен способ компиляции расширения, что в теории должно привести к ускорению процесса одобрения будущих обновлений магазинами.'
         }
