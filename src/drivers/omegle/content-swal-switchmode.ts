f0ab4a927fe0805aa7e623e5e3339c33a08ff518
qrlk <hi@qrlk.me>
2023-08-22 23:49:07 +0700

    comment out the non-existent image in switchModeOmegle

--- src/drivers/omegle/content-swal-switchmode.ts
+++ src/drivers/omegle/content-swal-switchmode.ts
@@ -4,7 +4,10 @@ export function switchModeOmegle() {
     let html = `<b>Please choose the mode that suits you best.</b><br><br>
  <form id="modeSelector">
  <input type="radio" id="minimalism" name="mode" value="minimalism">
- <label for="minimalism">simple (IP Locator & Dark Mode)<br><img src="${chrome.runtime.getURL('resources/img/' + '')}" style="border:1px solid; margin-top: 5px;"></label><br>
+ <label for="minimalism">simple (IP Locator & Dark Mode)
+<!-- TODO: add image for simple here-->
+<!-- <br><img src="${chrome.runtime.getURL('resources/img/' + '')}" style="border:1px solid; margin-top: 5px;">-->
+ </label><br>
  <br>
  <input type="radio" id="full" name="mode" value="full">
  <label for="full">advanced (full power, work-in-progress)</label></form>`
