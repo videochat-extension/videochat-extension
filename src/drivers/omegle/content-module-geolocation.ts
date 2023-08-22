adb467879db6afd1c5360b6e174ee4b8a4ca6d15
qrlk <hi@qrlk.me>
2023-08-23 05:56:24 +0700

    add target search hint to the omegle hints

--- src/drivers/omegle/content-module-geolocation.ts
+++ src/drivers/omegle/content-module-geolocation.ts
@@ -15,6 +15,7 @@ export class GeolocationModuleOmegle extends GeolocationModule {
 }
 
 export class ControlsTabApiOmegle extends ControlsTabApi {
+    //TODO: need to address DRY issue
     protected hintsDict = {
         "en": [
             {
@@ -25,6 +26,14 @@ export class ControlsTabApiOmegle extends ControlsTabApi {
                 enabled: true,
                 text: `The extension community is hosted on <a href=\"https://discord.gg/7DYWu5RF7Y\" target='_blank' style=\"text-decoration: none!important;\">Discord</a>.<br><br>On <a href=\"https://discord.gg/7DYWu5RF7Y\" target='_blank' style=\"text-decoration: none!important;\">our Discord server</a> you can subscribe to project updates, report a bug, suggest an idea, get technical help and discuss the extension with the developer and other users!`
             },
+            {
+                imgcontainer: "reviewImageContainer",
+                href: undefined,
+                src: undefined,
+                strength: 15,
+                enabled: (globalThis.platformSettings.get("enableTarget") && (globalThis.platformSettings.get("enableTargetCity") || globalThis.platformSettings.get("enableTargetRegion") || globalThis.platformSettings.get("enableTargetCountry"))),
+                text: `You have 'target search' enabled.<br><br>The extension will now skip everyone until it finds you someone from a country/region/city that you specified in the list in the settings.<br><br>You can disable this feature in the geolocation settings.`
+            },
         ],
         "ru": [
             {
@@ -34,6 +43,14 @@ export class ControlsTabApiOmegle extends ControlsTabApi {
                 strength: 4,
                 enabled: true,
                 text: `Сообщество расширения размещено в <a href=\"https://discord.gg/7DYWu5RF7Y\" target=\"_blank\" style=\"text-decoration: none!important;\">Discord</a>.<br><br>На <a href=\"https://discord.gg/7DYWu5RF7Y\" target=\"_blank\" style=\"text-decoration: none!important;\">нашем Discord сервере</a> вы можете следить за новостями проекта, сообщить об ошибке, предложить функцию, получить техническую помощь и обсудить расширение с разработчиком и другими пользователями!`
+            },
+            {
+                imgcontainer: "reviewImageContainer",
+                href: undefined,
+                src: undefined,
+                strength: 15,
+                enabled: (globalThis.platformSettings.get("enableTarget") && (globalThis.platformSettings.get("enableTargetCity") || globalThis.platformSettings.get("enableTargetRegion") || globalThis.platformSettings.get("enableTargetCountry"))),
+                text: `У вас включен таргетированный поиск.<br><br>Расширение будет пропускать всех ваших собеседников, пока не найдет кого-нибудь из списка стран/регионов/городов, который вы задали в настройках<br><br>Вы можете отключить эту функцию в настройках геолокации.'.`
             },
         ]
     }
